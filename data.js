d7d4f55c2004d80e327127f0b09470db1b837ab5
gian <giansingh4710@gmail.com>
2023-01-11 19:28:57 -0500

    changed data and can ascend and descend data

--- data.js
+++ data.js
@@ -1,8 +1,32 @@
 
 // pangti, timestamp, link
 const DATA = [
-  ["ਮਾਈ ਚਰਨ ਗੁਰ ਮੀਠੇ ॥" , "1:30" ,'http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Maaee%20Charan%20Gur%20Meethae.mp3'],
-  ["ਆਠ ਪਹਰ ਕਰ ਜੋੜਿ ਧਿਆਵਾ ॥", "8:30" ,"https://www.ikirtan.com//Bhai_Mohinder_Singh_Jee_SDO/Bhai_Mohinder_Singh_Jee_SDO_Jaagat_Jot_Jappae_%20Part_1.mp3"],
-  ["ਚੇਤਿ ਅਚੇਤ ਮੂੜ ਮਨ ਮੇਰੇ ਬਾਜੇ ਅਨਹਦ ਬਾਜਾ ॥", "26:20" ,"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Gur%20Sathigur%20Vich%20Naavai%20Kee%20Vaddee%20Vaddiaaee%202.mp3?fbclid=PAAaZiB6Ht5AJWLtiYOty7oTo-ATTiHkqmBAPtRKH8Xqh5L3iiXqbpxmR7j7A"],
-  ["ਦਿਨੁ ਰਾਤੀ ਆਰਾਧਹੁ ਪਿਆਰੋ ਨਿਮਖ ਨ ਕੀਜੈ ਢੀਲਾ ॥", "1:09:20" , "http://45.41.235.161/~daasstor/keertan/sdo/yt2_moga/609%20%20%20MGA%20SDO%201571%20%20%20Kab%20Ko%20Dhalle%20Ghhagroo%20%20%20Rakab%20Ganj%20Delhi%20%201989%20%20JS-po9xf0MlYCU.m4a"],
+  {
+    pangti:"ਮਾਈ ਚਰਨ ਗੁਰ ਮੀਠੇ ॥",
+    time:"1:30",
+    link:"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Maaee%20Charan%20Gur%20Meethae.mp3",
+    postNum:1,
+    keertani:"SDO Ji"
+  },
+  {
+    pangti:"ਆਠ ਪਹਰ ਕਰ ਜੋੜਿ ਧਿਆਵਾ ॥",
+    time:"8:30",
+    link:"https://www.ikirtan.com//Bhai_Mohinder_Singh_Jee_SDO/Bhai_Mohinder_Singh_Jee_SDO_Jaagat_Jot_Jappae_%20Part_1.mp3",
+    postNum:2,
+    keertani:"SDO Ji"
+  },
+  {
+    pangti:"ਚੇਤਿ ਅਚੇਤ ਮੂੜ ਮਨ ਮੇਰੇ ਬਾਜੇ ਅਨਹਦ ਬਾਜਾ ॥",
+    time:"26:20",
+    link:"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Gur%20Sathigur%20Vich%20Naavai%20Kee%20Vaddee%20Vaddiaaee%202.mp3?fbclid=PAAaZiB6Ht5AJWLtiYOty7oTo-ATTiHkqmBAPtRKH8Xqh5L3iiXqbpxmR7j7A",
+    postNum:3,
+    keertani:"SDO Ji"
+  },
+  {
+    pangti:"ਦਿਨੁ ਰਾਤੀ ਆਰਾਧਹੁ ਪਿਆਰੋ ਨਿਮਖ ਨ ਕੀਜੈ ਢੀਲਾ ॥",
+    time:"1:09:20",
+    link:"http://45.41.235.161/~daasstor/keertan/sdo/yt2_moga/609%20%20%20MGA%20SDO%201571%20%20%20Kab%20Ko%20Dhalle%20Ghhagroo%20%20%20Rakab%20Ganj%20Delhi%20%201989%20%20JS-po9xf0MlYCU.m4a",
+    postNum:4,
+    keertani:"SDO Ji",
+  },
 ]
