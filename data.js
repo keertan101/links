ec8958bb84053a71ff64c8203ba80a3334f47456
Gian Singh <73843250+Giansingh4710@users.noreply.github.com>
2023-01-12 01:02:05 -0500

    Update data.js 6 and made larrivar

--- data.js
+++ data.js
@@ -2,38 +2,46 @@
 // pangti, timestamp, link
 const DATA = [
   {
-    pangti:"ਮਾਈ ਚਰਨ ਗੁਰ ਮੀਠੇ ॥",
+    pangti:"ਮਾਈਚਰਨਗੁਰਮੀਠੇ ॥",
     time:"1:30",
     link:"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Maaee%20Charan%20Gur%20Meethae.mp3",
     postNum:1,
     keertani:"SDO Ji"
   },
   {
-    pangti:"ਆਠ ਪਹਰ ਕਰ ਜੋੜਿ ਧਿਆਵਾ ॥",
+    pangti:"ਆਠਪਹਰਕਰਜੋੜਿਧਿਆਵਾ ॥",
     time:"8:30",
     link:"https://www.ikirtan.com//Bhai_Mohinder_Singh_Jee_SDO/Bhai_Mohinder_Singh_Jee_SDO_Jaagat_Jot_Jappae_%20Part_1.mp3",
     postNum:2,
     keertani:"SDO Ji"
   },
   {
-    pangti:"ਚੇਤਿ ਅਚੇਤ ਮੂੜ ਮਨ ਮੇਰੇ ਬਾਜੇ ਅਨਹਦ ਬਾਜਾ ॥",
+    pangti:"ਚੇਤਿਅਚੇਤਮੂੜਮਨਮੇਰੇਬਾਜੇਅਨਹਦਬਾਜਾ ॥",
     time:"26:20",
     link:"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Gur%20Sathigur%20Vich%20Naavai%20Kee%20Vaddee%20Vaddiaaee%202.mp3?fbclid=PAAaZiB6Ht5AJWLtiYOty7oTo-ATTiHkqmBAPtRKH8Xqh5L3iiXqbpxmR7j7A",
     postNum:3,
     keertani:"SDO Ji"
   },
   {
-    pangti:"ਦਿਨੁ ਰਾਤੀ ਆਰਾਧਹੁ ਪਿਆਰੋ ਨਿਮਖ ਨ ਕੀਜੈ ਢੀਲਾ ॥",
+    pangti:"ਦਿਨੁਰਾਤੀਆਰਾਧਹੁਪਿਆਰੋਨਿਮਖਨਕੀਜੈਢੀਲਾ ॥",
     time:"1:09:20",
     link:"http://45.41.235.161/~daasstor/keertan/sdo/yt2_moga/609%20%20%20MGA%20SDO%201571%20%20%20Kab%20Ko%20Dhalle%20Ghhagroo%20%20%20Rakab%20Ganj%20Delhi%20%201989%20%20JS-po9xf0MlYCU.m4a",
     postNum:4,
     keertani:"SDO Ji",
   },
   {
-    pangti:"ਗੁਰਸਿਖ ਜੋਗੀ ਜਾਗਦੇ ਮਾਇਆ ਅੰਦਰਿ ਕਰਨਿ ਉਦਾਸੀ।",
+    pangti:"ਗੁਰਸਿਖਜੋਗੀਜਾਗਦੇਮਾਇਆਅੰਦਰਿਕਰਨਿਉਦਾਸੀ।",
     time:"2:09:30",
     link:"http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Tau%20kaarann%20saahibaa%20rang%20ratey.mp3",
     postNum:5,
     keertani:"SDO Ji",
-  }
+  },
+  {
+    pangti:"ਗੁਰਤੁਠੈਹਰਿਪਾਇਆਚੂਕੇਧਕਧਕੇ ॥",
+    time:"1:18:55",
+    link:"https://youtu.be/2o0dWoFMuXo",
+    postNum:6,
+    keertani:"Bhai Jasbir Singh Ji",
+  },
+
 ]
