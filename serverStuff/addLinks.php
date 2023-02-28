aebb1f82a261fe888270934570fc0dd6b3808422
giansingh4710 <giansingh4710@gmail.com>
2023-02-28 23:06:08 +0000

    fixing

--- serverStuff/addLinks.php
+++ serverStuff/addLinks.php
@@ -2,42 +2,44 @@
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
-echo "<pre>";
-var_dump($_POST);
-echo "</pre>";
+//echo "<pre>";
+//var_dump($_POST);
+//echo "</pre>";
 
-$timestamp = "";
-if(!empty($_POST["hours"])){
-  $timestamp = str_pad($_POST["hours"], 2, "0", STR_PAD_LEFT);
-}else{
-  $timestamp = "00";
+function addToCsv(){
+  $timestamp = "";
+  if(!empty($_POST["hours"])){
+    $timestamp = str_pad($_POST["hours"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp = "00";
+  }
+
+  $timestamp .= ":";
+  if(!empty($_POST["mins"])){
+    $timestamp .= str_pad($_POST["mins"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp .= "00";
+  }
+
+  $timestamp .= ":";
+  if(!empty($_POST["secs"])){
+    $timestamp .= str_pad($_POST["secs"], 2, "0", STR_PAD_LEFT);
+  }else{
+    $timestamp .= "00";
+  }
+
+  //"ਮਾਈਚਰਨਗੁਰਮੀਠੇ ॥", "1:30", "http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Maaee%20Charan%20Gur%20Meethae.mp3", "SDO Ji"
+  $line = array($_POST["pangti"], $timestamp, $_POST["link"],$_POST["keertani"]);
+
+  echo "<pre>";
+  var_dump($line);
+  echo "</pre>";
+
+  $fp = fopen("./rawData.csv", "a");
+  fputcsv($fp, $line); # $line is an array of strings (array|string[])
+  fclose($fp);
+  echo "The Data has been added to the csv file(database)";
 }
-
-$timestamp .= ":";
-if(!empty($_POST["mins"])){
-  $timestamp .= str_pad($_POST["mins"], 2, "0", STR_PAD_LEFT);
-}else{
-  $timestamp .= "00";
-}
-
-$timestamp .= ":";
-if(!empty($_POST["secs"])){
-  $timestamp .= str_pad($_POST["secs"], 2, "0", STR_PAD_LEFT);
-}else{
-  $timestamp .= "00";
-}
-
-//"ਮਾਈਚਰਨਗੁਰਮੀਠੇ ॥", "1:30", "http://sikhsoul.com/golden_khajana_files/mp3/Keertan/Bhai%20Mohinder%20Singh%20SDO/Maaee%20Charan%20Gur%20Meethae.mp3", "SDO Ji"
-$line = array($_POST["pangti"], $timestamp, $_POST["link"],$_POST["keertani"]);
-
-echo "<pre>";
-var_dump($line);
-echo "</pre>";
-
-$fp = fopen("./rawData.csv", "a");
-fputcsv($fp, $line); # $line is an array of strings (array|string[])
-fclose($fp);
-echo "The Data has been added to the csv file(database)";
 
 function makeDataToJs($file){
   $objLst = array();
@@ -57,6 +59,7 @@ function makeDataToJs($file){
   return json_encode($objLst);
 }
 
+addToCsvFromPostRequest();
 $jsObj = makeDataToJs("./rawData.csv");
 $myFile = fopen("./data.js", "w");
 fwrite($myFile,"const DATA = ");
